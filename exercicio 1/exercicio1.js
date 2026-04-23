f69c6e259ec9259b130fc488894d61911ce10a53
guilherme-smdev <guiilherme.profissional27@gmail.com>
2026-04-22 21:21:13 -0400

    atividades

---
+++ exercicio 1/exercicio1.js
@@ -0,0 +1,11 @@
+let nome = "Guilherme Moreira"
+let idade = 23
+let valorlogico = true
+
+console.log(nome)
+console.log(typeof nome)
+console.log(idade)
+console.log(typeof idade)
+console.log(valorlogico)
+console.log(typeof valorlogico)
+console.log("Olá, meu nome é "+nome+", tenho "+idade+" anos, e estudo no IFMT.")
---
+++ exercicio 2/exercicio2.js
@@ -0,0 +1,7 @@
+let a = "10"
+let b = 5
+
+console.log(typeof a)
+console.log(typeof b)
+console.log(a + b)
+console.log(Number(a)+b)
